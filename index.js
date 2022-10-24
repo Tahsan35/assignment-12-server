d2267247596cfbb8667d5c300f07a6edce17bcb4
Tahsan <tahsanknmas14@gmail.com>
2022-10-24 15:01:33 +0600

    connect data base

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+
+const app = express()
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -9,3 +16,26 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a6zbr2z.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const partsCollection = client.db('webTech-House').collection('parts');
+
+        // get all parts
+        app.get('/parts', async (req, res) => {
+            const query = {};
+            const result = await partsCollection.find(query).toArray();
+            res.send(result);
+        })
+        console.log('database connected');
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
