dcc0f9cb27244e573efd40873e2057906a9b9ded
Tahsan <tahsanknmas14@gmail.com>
2022-10-25 10:37:32 +0600

    set up jsonToken

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express()
@@ -19,8 +19,6 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a6zbr2z.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
@@ -28,6 +26,23 @@ async function run() {
         await client.connect();
         const partsCollection = client.db('webTech-House').collection('parts');
         const usersCollection = client.db('webTech-House').collection('users');
+
+
+        // function for user access
+        function verifyToken(req, res, next) {
+            const authHeader = req.headers.authorization;
+            if (!authHeader) {
+                return res.status(401).send({ message: 'UnAuthorized access' })
+            }
+            const token = authHeader.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+                if (err) {
+                    return res.status(403).send({ message: 'Forbidden access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
 
         // get all parts
         app.get('/parts', async (req, res) => {
